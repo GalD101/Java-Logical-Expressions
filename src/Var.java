502fa457e93ff9039ce219cc45b5585a7cbcf7ea
GalD101 <galdali10@gmail.com>
2024-06-18 12:08:36 +0300

    this commit message has been replaced due to foul language.

--- src/Var.java
+++ src/Var.java
@@ -1,13 +1,55 @@
 package src;
 
-public class Var {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Var implements Expression {
     private String name;
 
     public Var(String name) {
         this.name = name;
     }
 
-    public String getName() {
+    // Evaluate the expression using the variable values provided
+    // in the assignment, and return the result. If the expression
+    // contains a variable which is not in the assignment, an exception
+    // is thrown.
+    public Boolean evaluate(Map<String, Boolean> assignment) throws Exception {
+        if (!assignment.containsKey(this.name)) {
+            this.evaluate(); // throw new Exception("Variable not found in assignment");
+        }
+
+        return assignment.get(this.name);
+    }
+
+    // A convenience method. Like the `evaluate(assignment)` method above,
+    // but uses an empty assignment.
+    public Boolean evaluate() throws Exception {
+        throw new Exception("Variable not found in assignment");
+    }
+
+    // Returns a list of the variables in the expression.
+    public List<String> getVariables() {
+        ArrayList<String> variables = new ArrayList<String>();
+        variables.add(this.name);
+        return new ArrayList<>();
+    }
+
+    // Returns a nice string representation of the expression.
+    @Override
+    public String toString() {
         return this.name;
+    }
+
+    // Returns a new expression in which all occurrences of the variable
+    // var are replaced with the provided expression (Does not modify the
+    // current expression).
+    public Expression assign(String var, Expression expression) {
+        if (var.equals(this.name)) {
+            return expression;
+        }
+
+        return this;
     }
 }
