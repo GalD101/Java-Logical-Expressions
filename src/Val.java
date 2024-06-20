9bac041bda66b7943e13b756be70e309cc828720
GalD101 <galdali10@gmail.com>
2024-06-20 20:18:43 +0300

    Val and Var

--- src/Val.java
+++ src/Val.java
@@ -6,11 +6,9 @@ import java.util.Map;
 
 public class Val implements Expression {
     private Boolean value;
-    private String name;
 
     public Val(Boolean value) {
         this.value = value;
-        this.name = value.toString();
     }
 
     // Evaluate the expression using the variable values provided
@@ -18,23 +16,17 @@ public class Val implements Expression {
     // contains a variable which is not in the assignment, an exception
     // is thrown.
     public Boolean evaluate(Map<String, Boolean> assignment) throws Exception {
-        if (!assignment.containsKey(this.name)) {
-            this.evaluate(); // throw new Exception("Variable not found in assignment");
-        }
-
-        return assignment.get(this.name);
+        return this.evaluate();
     }
 
     // A convenience method. Like the `evaluate(assignment)` method above,
     // but uses an empty assignment.
     public Boolean evaluate() throws Exception {
-        throw new Exception("Variable not found in assignment");
+        return this.value;
     }
 
     // Returns a list of the variables in the expression.
     public List<String> getVariables() {
-        ArrayList<String> variables = new ArrayList<String>();
-        variables.add(this.value.toString());
         return new ArrayList<>();
     }
 
@@ -50,12 +42,8 @@ public class Val implements Expression {
 
     // Returns a new expression in which all occurrences of the variable
     // var are replaced with the provided expression (Does not modify the
-    // current expression).
+    // current expression). // TODO: Check if this is correct
     public Expression assign(String var, Expression expression) {
-        if (var.equals(this.name)) {
-            return expression;
-        }
-
         return this;
     }
 }
